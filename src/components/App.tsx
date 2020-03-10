fe13bb7efb3e4194a20dc5fd40eb3c437a3748de
Jeremy Dahms <jdahms@vertafore.com>
2020-03-10 12:23:15 -0600

    last commit of initial setup, removed unused file HelloWorld.tsx and fixed linting error in App.tsx due to lack of return type

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 
 import PageInterface from '../PageInterface';
 
-const App: React.FunctionComponent<PageInterface> = () => {
-    return (
-        <div>
-            <h1>Welcome to React with TypeScript</h1>
-            <p>The color of this page is: {this.props.color}</p>
-        </div>
-    );
-};
+class App extends React.Component<PageInterface, {}> {
+    render(): JSX.Element {
+        return (
+            <div>
+                <h1>Welcome to React with Typescript</h1>
+                <p>The color of this page is: {this.props.color}</p>
+            </div>
+        );
+    }
+}
 
 export default App;
